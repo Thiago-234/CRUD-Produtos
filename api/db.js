e0fcc433cb10478a6207c963d49b3d5018e8a543
Seu NomeThiago-234 <camposthiago2021@gmail.com>
2025-09-03 22:38:17 -0300

    Inicio do projeto

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "mysqlnopc123#",
-    database: "produtos"
+    user: "usuariodb",
+    password: "senhadb",
+    database: "nomedb"
 })
